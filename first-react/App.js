fc2c25e0aa54bed7f0bb81aa744c122e06456e41
Marco T Barros <marcotulio.barros@uel.br>
2023-04-12 11:21:16 -0300

    New repository for mobile apps in JavaScript and first two projects

---
+++ first-react/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+
+const PlaceholderImage = require('./assets/background-image.png');
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer placeholderImageSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button theme="primary" label="Escolha uma foto" />
+                <Button label="Usar essa foto" />
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+      },    
+});
